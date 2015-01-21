5bd2f6cb32adfb9f96bd52bb91a3ddfa91175d0a
Stuart Mackle <stuartmackle@Stuarts-MacBook-Pro.local>
2015-01-21 21:23:43 +0000

    Chapter 4 - Complete

--- tango/rango/views.py
+++ tango/rango/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Rango says hey there world!")
-
+    return HttpResponse("Rango says: Hello world! <br/> <a href='/rango/about'>About</a>")
+    
 def about(request):
-    return HttpResponse("This tutorial has put together by Stuart Mackle, 2095332")
+    return HttpResponse("This tutorial has put together by Stuart Mackle, 2095332 <a href='/rango/'>Index</a>  ")
