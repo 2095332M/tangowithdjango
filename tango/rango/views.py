316af8acb2e1792789773dfe40b433ef4e10d2cc
Stuart Mackle <stuartmackle@glaroam2-171-91.wireless.gla.ac.uk>
2015-02-03 15:47:35 +0000

    Chapter 8 - Completed

--- tango/rango/views.py
+++ tango/rango/views.py
@@ -5,8 +5,59 @@ from django.http import HttpResponse
 from rango.models import Category
 from rango.models import Page
 
-def category(request, category_name_slug):
+from rango.forms import CategoryForm
+from rango.forms import PageForm
 
+def add_page(request, category_name_slug):
+
+    try:
+        cat = Category.objects.get(slug=category_name_slug)
+    except Category.DoesNotExist:
+                cat = None
+
+    if request.method == 'POST':
+        form = PageForm(request.POST)
+        if form.is_valid():
+            if cat:
+                page = form.save(commit=False)
+                page.category = cat
+                page.views = 0
+                page.save()
+                return category(request, category_name_slug)
+        else:
+            print form.errors
+    else:
+        form = PageForm()
+
+    context_dict = {'form':form, 'category': cat}
+
+    return render(request, 'rango/add_page.html', context_dict)
+
+def add_category(request):
+    # A HTTP POST?
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+
+        # Have we been provided with a valid form?
+        if form.is_valid():
+            # Save the new category to the database.
+            form.save(commit=True)
+
+            # Now call the index() view.
+            # The user will be shown the homepage.
+            return index(request)
+        else:
+            # The supplied form contained errors - just print them to the terminal.
+            print form.errors
+    else:
+        # If the request was not a POST, display the form to enter details.
+        form = CategoryForm()
+
+    # Bad form (or form details), no form supplied...
+    # Render the form with error messages (if any).
+    return render(request, 'rango/add_category.html', {'form': form})
+
+def category(request, category_name_slug):
     # Create a context dictionary which we can pass to the template rendering engine.
     context_dict = {}
 
@@ -20,14 +71,14 @@ def category(request, category_name_slug):
         context_dict['category'] = category
     except Category.DoesNotExist:
         pass
-
+    context_dict['category_name_slug'] = category.slug
     return render(request, 'rango/category.html', context_dict)
 
 
 def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
     pages_list = Page.objects.order_by('-views')[:5]
-    context_dict = {'categories': category_list, 'pages': pages_list}
+    context_dict = {'categories': category_list, 'pages': pages_list,}
     
     return render(request, 'rango/index.html', context_dict)    
 
